a27228daaba2970ee91d415eab649ffeb02810d5
Tyler Dickens <tyler.william.dickens@gmail.com>
2022-10-18 23:58:01 -0700

    Remove hardcode secret key

--- mlpmachine/settings.py
+++ mlpmachine/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pxypcwu2vtm@97^8u!-5m@^=0smqd8#e$lv7h9^6ctw^a68(n3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
