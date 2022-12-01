160c046fd697535cbd978be4865f6ebb665fd6bc
tdickens <tyler.william.dickens@gmail.com>
2022-11-30 22:01:13 -0800

    Removed read buttons and added portfolio

--- mlpmachine/settings.py
+++ mlpmachine/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+# DEBUG = True
+# SECRET_KEY = 111
 ALLOWED_HOSTS = []
 
 
